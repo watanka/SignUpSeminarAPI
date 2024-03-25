b1339438e003d9625969677e241a9409e32f7965
eunsung <eunsung.shin@gmail.com>
2024-03-25 22:32:30 +0900

    api 입력형태(body) 수정

--- src/test/java/com/SeminarRegistration/seminarRegistration/RegistrationControllerTest.java
+++ src/test/java/com/SeminarRegistration/seminarRegistration/RegistrationControllerTest.java
@@ -1,56 +1,61 @@
 package com.SeminarRegistration.seminarRegistration;
 
+import com.SeminarRegistration.seminarRegistration.repository.JpaRegistrationRepository;
+import com.SeminarRegistration.seminarRegistration.repository.MemoryRegistrationRepository;
 import com.SeminarRegistration.seminarRegistration.service.RegistrationService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 @Transactional
 class RegistrationControllerTest {
 
-    @Autowired
-    RegistrationController registrationController;
 
-    @MockBean
-    RegistrationService registrationService;
+    @Autowired RegistrationController registrationController;
+    @Autowired RegistrationService registrationService;
+    @Autowired JpaRegistrationRepository jpaRegistrationRepository;
 
-    @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(registrationController).build();
+    }
 
 
-//    @Test
-//    @DisplayName("세미나 신청 성공 테스트")
-//    public void signUpSeminar() throws Exception {
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.post("/seminar/")
-//                        .content(String.valueOf(id))
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                )
-//                .andExpect(status().isOk())
-//                .andExpect(content().string("요청 성공"));
-//    }
+    @Test
+    @DisplayName("세미나 신청 성공 테스트")
+    public void signUpSeminar() throws Exception {
+
+        long seminarId = 1L;
+        String userId = "eunsung";
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("seminarId", seminarId);
+        jsonObject.put("userId", userId);
+        String requestBody = jsonObject.toString();
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/seminar/")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().string("요청 성공"));
+    }
 
     @Test
     @DisplayName("세미나 신청 실패")
