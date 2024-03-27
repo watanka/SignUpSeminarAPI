8d1341ed5e61076256cfbc9e21bf9154ab21c529
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-03-27 17:56:10 +0900

    디렉터리 구조 변경

--- src/main/java/com/SeminarRegistration/RegistrationController.java
+++ src/main/java/com/SeminarRegistration/RegistrationController.java
@@ -1,7 +1,7 @@
-package com.SeminarRegistration.registration;
+package com.SeminarRegistration;
 
 import com.SeminarRegistration.domain.AppUser;
-import com.SeminarRegistration.registration.RegistrationService;
+import com.SeminarRegistration.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
--- src/test/java/com/SeminarRegistration/SeminarRegistrationApplicationTests.java
+++ src/test/java/com/SeminarRegistration/SeminarRegistrationApplicationTests.java
@@ -1,13 +1,13 @@
-package com.SeminarRegistration;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SeminarRegistrationApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.SeminarRegistration;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SeminarRegistrationApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
